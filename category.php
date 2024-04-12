c4868bcdde44349830de2329983fb3f242800699
Robert-math <68157423+Robert-math@users.noreply.github.com>
2024-04-12 19:39:21 +0400

    Category page pagination added

--- category.php
+++ category.php
@@ -10,48 +10,60 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<main id="primary" class="site-main">
 
-		<?php if ( have_posts() ) : ?>
+	<?php if (have_posts()): ?>
 
-			<div class="projects_title_container">
-				<?php
-				add_filter('get_the_archive_title_prefix','__return_false');
-				?>
-				<h1 class="projects_title"><?php echo get_the_archive_title(); ?></h1>
-                <h3 class="projects_subtitle"><?php echo get_the_archive_description();?></h3>
-			</div>
-			<div class="projects_items_container">
-				
+		<div class="projects_title_container">
 			<?php
-			while ( have_posts() ) :
+			add_filter('get_the_archive_title_prefix', '__return_false');
+			?>
+			<h1 class="projects_title"><?php echo get_the_archive_title(); ?></h1>
+			<h3 class="projects_subtitle"><?php echo get_the_archive_description(); ?></h3>
+		</div>
+		<div class="projects_items_container">
+
+			<?php
+			while (have_posts()):
 				the_post();
 				?>
 				<div class="projects_item">
-                    <h3 class="projects_item_title"><?php the_title(); ?></h3>
-                    <div class="projects_item_flex">
-					    <div class="projects_item_img"><?php the_post_thumbnail();?></div>
-                        <div class="projects_item_excerpt"><p><?php the_excerpt(); ?></p></div>
+					<h3 class="projects_item_title"><?php the_title(); ?></h3>
+					<div class="projects_item_flex">
+						<div class="projects_item_img"><?php the_post_thumbnail(); ?></div>
+						<div class="projects_item_excerpt">
+							<p><?php the_excerpt(); ?></p>
+						</div>
 					</div>
-                    <div class="projects_item_flex2">
-                    <a href="<?php the_permalink();?>" class="projects_item_readmore"><span><?php echo get_theme_mod('readMoreButton')?></span> <i class="fa-solid fa-caret-right"></i></a>
-                    </div>
-                </div>
+					<div class="projects_item_flex2">
+						<a href="<?php the_permalink(); ?>"
+							class="projects_item_readmore"><span><?php echo get_theme_mod('readMoreButton') ?></span> <i
+								class="fa-solid fa-caret-right"></i></a>
+					</div>
+				</div>
 
-				<?php endwhile;?>
-				
+			<?php endwhile; ?>
+			<div class="projects_pagination">
+				<?php the_posts_pagination(
+					array(
+						'mid_size' =>10,
+						'prev_text' => __('<', 'textdomain'),
+						'next_text' => __('>', 'textdomain'),
+					)
+				); ?>
 			</div>
+		</div>
 
 
-			<?php
-		else :
+		<?php
+	else:
 
-			get_template_part( 'template-parts/content', 'none' );
+		get_template_part('template-parts/content', 'none');
 
-		endif;
-		?>
+	endif;
+	?>
 
-	</main><!-- #main -->
+</main><!-- #main -->
 
 <?php
 
